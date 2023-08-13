c9dfe15d7cc5901796ed7e2959e996c48a1a967d
Vairava Naresh <vairavanareshganesan@gmail.com>
2023-08-13 23:45:11 +0530

    added models and serializers

--- buyit/ecomm/models.py
+++ buyit/ecomm/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class OrderModel(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    product_name = models.CharField(max_length=200)
+    username = models.CharField(max_length=50)
+    delivery_data = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.product_name, self.order_id
+
+
+class ProductModel(models.Model):
+    product_name = models.CharField(max_length=200)
+    stock_count = models.IntegerField()
+    order_id = models.ForeignKey(OrderModel, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.product_name, self.stock_count
+
+
+class UserModel(models.Model):
+    username =  models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=50,)
+    is_admin = models.BooleanField()
+    is_seller = models.BooleanField()
+    order_id = models.ForeignKey(OrderModel, on_delete=models.CASCADE)
+    phone = models.IntegerField(null=False, unique=True)
+    address = models.TextField(null=False)
+
+    def __str__(self):
+        return self.username
---
+++ buyit/ecomm/serilalizers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from ecomm import models
+
+class OrderSerilalizer(serializers.ModelSerializer):
+    class Meta:
+        model = models.OrderModel
+        fields = '__all__'
+
+    
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.ProductModel
+        fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.UserModel
+        fields = '__all__'
